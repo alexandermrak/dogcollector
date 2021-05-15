bdcfeb951d4b1e6f7a63151c4b1580a3fd567619
alexandermak <alexandermrak23@gmail.com>
2021-05-15 12:45:29 -0500

    Finishes part 1 of Django App: Dog Collector

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-# Create your views here.
+# Create your classes here.
+class Dog: 
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+dogs = [
+  Dog('Moose', 'Mut', 'Lovable & Carefree', 5),
+  Dog('Rocco', 'Mut', 'Introverted & Stubborn', 5),
+]
 
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello Doggies</h1>')
 
 def about(request):
-    return HttpResponse('<h1>ABOUT</h1>')
+  return render(request, 'about.html')
+
+def dogs_index(request):
+  return render(request, 'dogs/index.html', {'dogs': dogs})
