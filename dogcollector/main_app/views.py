9e0bfe82613688a821710f478c71d0a91e73f351
alexandermak <alexandermrak23@gmail.com>
2021-05-22 19:10:16 -0500

    finishes lab (done via coding along the lecture videos)

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,25 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Dog, Toy
+from .forms import FeedingForm
 
-from django.http import HttpResponse
-# Create your classes here.
-class Dog: 
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'description', 'age']
 
-dogs = [
-  Dog('Moose', 'Mut', 'Lovable & Carefree', 5),
-  Dog('Rocco', 'Mut', 'Introverted & Stubborn', 5),
-]
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
 
-# Define the home view
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
+
 def home(request):
-  return HttpResponse('<h1>Hello Doggies</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def dogs_index(request):
-  return render(request, 'dogs/index.html', {'dogs': dogs})
+  dogs = Dog.objects.all()
+  return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+  dog = Dog.objects.get(id=dog_id)
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in=dog.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {
+    'dog': dog, 'feeding_form': feeding_form,
+    'toys': toys_dog_doesnt_have
+  })
+
+def add_feeding(request, dog_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+def unassoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
